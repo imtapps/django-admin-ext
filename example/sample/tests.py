79434a5d7c3a09d5e524b1dcb963d1b57b21cd8a
Henry <toran.billups@theimtgroup.com>
2012-08-07 15:19:07 -0500

    added some retry logic and better timeout handling to "hopefully" stabilize the selenium tests

--- example/sample/tests.py
+++ example/sample/tests.py
@@ -1,10 +1,43 @@
+from functools import wraps
 from django.contrib.auth.models import User
 from django.test.testcases import TestCase, LiveServerTestCase
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException, NoSuchElementException, StaleElementReferenceException
 from selenium.webdriver.firefox.webdriver import WebDriver
-import time
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+def retry(func):
+
+    @wraps(func)
+    def retry_test(self, countdown=10, *args, **kwargs):
+        try:
+            result = func(self, *args, **kwargs)
+        except Exception:
+            if countdown <= 0:
+                raise
+
+            self.tearDown()
+            self._post_teardown()
+            self._pre_setup()
+            self.setUp()
+            result = retry_test(self, countdown=countdown - 1, *args, **kwargs)
+
+        return result
+
+    return retry_test
+
+
+class Retry(type):
+
+    def __new__(cls, name, bases, attrs):
+        for test in filter(lambda i: i.startswith('test_'), attrs):
+            attrs[test] = retry(attrs[test])
+
+        return super(Retry, cls).__new__(cls, name, bases, attrs)
+
 
 class AjaxAdminTests(TestCase, LiveServerTestCase):
+    __metaclass__ = Retry
 
     @classmethod
     def setUpClass(cls):
@@ -20,43 +53,57 @@ class AjaxAdminTests(TestCase, LiveServerTestCase):
         list(User.objects.all())
         self.login()
 
+    def _get_element(self, context, method, argument):
+        return getattr(context, method)(argument)
+
+    def find_element(self, context=None, name=None, selector=None, tag=None):
+        argument = name or selector or tag
+        context = context or self.browser
+        if name:
+            method = 'find_element_by_name'
+        elif selector:
+            method = 'find_element_by_css_selector'
+        elif tag:
+            method = 'find_elements_by_tag_name'
+        else:
+            raise Exception("No Selector")
+
+        WebDriverWait(context, 5, 1).until(lambda d: self._get_element(d, method, argument))
+        return self._get_element(context, method, argument)
+
+    def click_element(self, **kwargs):
+        element = self.find_element(**kwargs)
+        element.click()
+
     def login(self):
         self.browser.get("%s/admin/" % self.live_server_url)
-        user = self.browser.find_element_by_css_selector('#id_username')
+        user = self.find_element(selector='#id_username')
         user.send_keys("admin")
-        pswd = self.browser.find_element_by_css_selector('#id_password')
+        pswd = self.find_element(selector='#id_password')
         pswd.send_keys("test")
         self.click_element(selector=".submit-row>[type='submit']")
 
-    def click_element(self, name=None, selector=None):
-        element = None
-        if name:
-            element = self.browser.find_element_by_name(name)
-        elif selector:
-            element = self.browser.find_element_by_css_selector(selector)
-        element.click()
-        time.sleep(2)
-
     def assert_selected_option(self, element_id, value):
-        option = self.browser.find_element_by_css_selector('#' + element_id + ' option[selected="selected"]')
+        option = self.find_element(selector='#' + element_id + ' option[selected="selected"]')
         self.assertEqual(value, option.text)
 
     def assert_select_has_options(self, element_id, expected_ingredients):
-        details = self.browser.find_element_by_css_selector('#' + element_id)
-        options = details.find_elements_by_tag_name('option')
+        details = self.find_element(selector='#' + element_id)
+        options = self.find_element(context=details, tag='option')
         self.assertItemsEqual(expected_ingredients, [o.text for o in options])
 
     def change_value_for_element(self, element_id, value):
-        element = self.browser.find_element_by_css_selector('#' + element_id)
+        element = self.find_element(selector='#' + element_id)
         element.send_keys(value)
         # click off of the element to trigger the change event
         self.click_element(selector='label[for="' + element_id + '"]')
 
     def test_main_ingredient_element_not_present_initially(self):
         self.browser.get("%s/admin/sample/meal/add/" % self.live_server_url)
-        self.browser.find_element_by_css_selector('#id_food_type')
-        with self.assertRaises(NoSuchElementException):
-            self.browser.find_element_by_css_selector('#id_main_ingredient')
+
+        self.find_element(selector='#id_food_type')
+        with self.assertRaises(TimeoutException):
+            self.find_element(selector='#id_main_ingredient')
 
     def test_main_ingredient_element_shows_when_pizza_food_type_is_selected(self):
         self.browser.get("%s/admin/sample/meal/add/" % self.live_server_url)
@@ -83,13 +130,17 @@ class AjaxAdminTests(TestCase, LiveServerTestCase):
         self.change_value_for_element('id_food_type', 'burger')
         self.change_value_for_element('id_main_ingredient', 'beef')
 
-        details = self.browser.find_element_by_css_selector('#id_ingredient_details')
+        details = self.find_element(selector='#id_ingredient_details')
         self.assertTrue(details.is_displayed())
 
         self.change_value_for_element('id_main_ingredient', 'lettuce')
 
-        with self.assertRaises(NoSuchElementException):
-            self.browser.find_element_by_css_selector('#id_ingredient_details')
+        try:
+            self.find_element(selector='#id_ingredient_details')
+        except (NoSuchElementException, TimeoutException, StaleElementReferenceException):
+            pass
+        else:
+            self.fail("Expected not to find #id_ingredient_details")
 
     def test_ingredient_details_change_when_main_ingredient_changes(self):
         self.browser.get("%s/admin/sample/meal/add/" % self.live_server_url)
@@ -133,18 +184,18 @@ class AjaxAdminTests(TestCase, LiveServerTestCase):
         self.change_value_for_element('id_food_type', 'burger')
 
         # create new meal
-        main_ingredient = self.browser.find_element_by_css_selector('#id_main_ingredient')
+        main_ingredient = self.find_element(selector='#id_main_ingredient')
         main_ingredient.send_keys('mushrooms')
         self.click_element(name='_continue')
 
         # change main_ingredient for new meal
-        main_ingredient2 = self.browser.find_element_by_css_selector('#id_main_ingredient')
+        main_ingredient2 = self.find_element(selector='#id_main_ingredient')
         main_ingredient2.send_keys('lettuce')
         self.click_element(name='_continue')
 
         # make sure there are no errors
-        with self.assertRaises(NoSuchElementException):
-            self.browser.find_element_by_css_selector(".errors")
+        with self.assertRaises(TimeoutException):
+            self.find_element(selector=".errors")
 
         # make sure our new main_ingredient was saved
         self.assert_selected_option('id_main_ingredient', 'lettuce')
@@ -155,10 +206,10 @@ class AjaxAdminTests(TestCase, LiveServerTestCase):
 
     def test_gives_field_required_error_when_dynamic_field_not_chosen(self):
         self.browser.get("%s/admin/sample/meal/add/" % self.live_server_url)
-        food_type = self.browser.find_element_by_css_selector('#id_food_type')
+        food_type = self.find_element(selector='#id_food_type')
         food_type.send_keys('burger')
 
         self.click_element(name='_save')
 
-        error_item = self.browser.find_element_by_css_selector(".errors.field-main_ingredient li")
+        error_item = self.find_element(selector=".errors.field-main_ingredient li")
         self.assertEqual("This field is required.", error_item.text)
