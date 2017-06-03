001d18a8cb170af5e8772c1ef0186287fe1dd5cf
Matthew J Morrison <mattjmorrison@mattjmorrison.com>
2017-06-02 20:54:45 -0500

    Debugging: Updated timeout in tests to wait longer

--- example/sample/tests.py
+++ example/sample/tests.py
@@ -81,7 +81,7 @@ class AjaxAdminTests(TestCase, StaticLiveServerTestCase):
         else:
             raise Exception("No Selector")
 
-        WebDriverWait(context, 5, 1).until(lambda d: self._get_element(d, method, argument))
+        WebDriverWait(context, 60, 1).until(lambda d: self._get_element(d, method, argument))
         return self._get_element(context, method, argument)
 
     def click_element(self, **kwargs):
